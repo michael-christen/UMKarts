cb9011fbb440afed5f6a139960e00f4e9b481dd3
karatekid <mikechri@umich.edu>
2013-11-22 21:20:17 -0500

    Got the listener setup, just need to add some parsing stuff

---
+++ listener/serialHW/app.js
@@ -0,0 +1,140 @@
+var fs = require('fs')
+    , http = require('http')
+    , socketio = require('socket.io')
+    , serialport = require('serialport')
+    , static = require('node-static')
+    , util   = require('util')
+    , path   = require('path');
+
+//var folder = new(static.Server)(__dirname + '/static');
+
+var server = http.createServer(function(request, response) {
+    console.log('request starting...');
+    var filePath = '.' + request.url;
+    if (filePath == './') {
+	filePath = './index.html';
+    }
+		
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+	case '.js':
+		contentType = 'text/javascript';
+		break;
+	case '.css':
+		contentType = 'text/css';
+		break;
+    }
+    path.exists(filePath, function(exists) {
+	if (exists) {
+	    fs.readFile(filePath, function(error, content) {
+		if (error) {
+		    response.writeHead(500);
+		    response.end();
+		} else {
+		    response.writeHead(200, { 'Content-Type': contentType });
+		    response.end(content, 'utf-8');
+		}
+	    });
+	} else {
+	    response.writeHead(404);
+	    response.end();
+	}
+    });
+	/*
+
+var server = http.createServer(function(req, res) {
+    res.writeHead(200, { 'Content-type': 'text/html'});
+    res.end(fs.readFileSync(__dirname + '/index.html'));
+    req.addListener('end', function() {
+    folder.serve(req, res, function(err, result) {
+	if (err) {
+	    console.error('Error serving %s - %s',
+	    req.url, err.message);
+	    if (err.status === 404 || err.status === 500) {
+		folder.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+	    } else {
+		res.writeHead(err.status, err.headers);
+		res.end();
+	    }
+	} else {
+	    console.log('%s - %s', req.url, res.message); 
+	}
+});
+*/
+    /*
+    req.addListener('end', function() {
+	folder.serve(req,res);
+    });
+    */
+}).listen(8080, function() {
+    console.log('Listening at: http://localhost:8080');
+});
+
+    /*`
+var server = http.createServer(app);
+server.listen(8080, function() {
+    console.log('Listening at: http://localhost:8080');
+});
+app.use(express.static(__dirname+"/jQuery"));
+app.use(express.static(__dirname+"/bootstrap"));
+*/
+/*
+var SerialPort = serialport.SerialPort;
+var serialPort = new SerialPort("/dev/ttyUSB0", {
+    baudrate: 57600,
+    databits: 8,
+    stopbits: 1,
+    parity: 'none',
+    parser: serialport.parsers.readline("\n")
+});
+*/
+var socket = socketio.listen(server);
+var numConnections = 0;
+var clients = new Object();
+
+socket.on('connection', function(client){ 
+    console.log("Id: " + client.id);
+    clients[client.id] = client;
+    client.send("hello world");
+    client.on('message', function (msg) {
+	console.log('Message Received: ', msg);
+	client.broadcast.emit('message', msg);
+    });
+    /*
+    client.on('disconnect', function () {
+	delete clients[client.id];
+    });
+    */
+});
+/*
+socketio.listen(server).on('connection', function (socketA) {
+    socketA.on('message', function (msg) {
+	console.log('Message Received: ', msg);
+	socket.broadcast.emit('message', msg);
+    });
+
+});
+*/
+/*
+    serialPort.on("open", function() {
+	console.log('open serial');
+	serialPort.on('data', function(data) {
+	    //console.log('data received: ' + data);
+	    //console.log('len = ' + data.length);
+	    for(var c in clients) {
+		if (clients.hasOwnProperty(c)) {
+		    for(var cl in clients) {
+		        console.log(cl + ' ' + clients[cl]);
+		    }
+		    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@');
+		    console.log('length = ' + data.length);
+		    console.log('msg = ' + data);
+		    clients[c].broadcast.emit('message', ' ' + data);
+		    //Only go through once
+		    break;
+		}
+	    }
+	});
+    });
+*/
---
+++ listener/serialHW/static/js/umkart.js
@@ -0,0 +1,213 @@
+$(function(){
+    //Take care of socket connections
+    var iosocket = io.connect();
+    iosocket.on('connect', function () {
+	$('#incomingChatMessages').append($('<li>Connected</li>'));
+	iosocket.on('message', function(message) {
+	    $('#incomingChatMessages').append($('<li></li>').text(message));
+	});
+	iosocket.on('disconnect', function() {
+	    $('#incomingChatMessages').append('<li>Disconnected</li>');
+	});
+    });
+    $('#outgoingChatMessage').keypress(function(event) {
+	if(event.which == 13) {
+	    event.preventDefault();
+	    iosocket.send($('#outgoingChatMessage').val());
+	    $('#incomingChatMessages').append($('<li></li>').text($('#outgoingChatMessage').val()));
+	    $('#outgoingChatMessage').val('');
+	}
+    });
+
+    //Creation of div elements and such
+    var numPlayers = 0;
+    var selectedPlayerId = null;
+    var ItemEnum = {
+	'GREEN_SHELL': {
+	    name:"Green Shell",
+	    img:"/static/media/Item_greenshell.png"
+	 },
+	'RED_SHELL': {
+	    name:"Red Shell",
+	    img:"/static/media/Item_redshell.png"
+	 },
+	'BLUE_SHELL': {
+	    name:"Blue Shell",
+	    img:"/static/media/Item_blueshell.png"
+	 },
+	'MUSHROOM': {
+	    name:"Mushroom",
+	    img:"/static/media/Item_mushroom.png"
+	 },
+	'SUPER_MUSHROOM': {
+	    name:"Super Mushroom",
+	    img:"/static/media/Item_supermushroom.png"
+	 },
+	'STAR': {
+	    name:"Star",
+	    img:"/static/media/Item_star.png"
+	 },
+	'EMPTY': {
+	    name: "none",
+	    img:"/static/media/Item_empty.png"
+	}
+    };
+    function getItemById(id) {
+	return ItemEnum[id];
+    }
+    var CharacterEnum = {
+	'MARIO': {
+	    img: "/static/media/Mario_inactive.png",
+            name: "Mario"
+	},
+	'LUIGI': {
+	    img: "/static/media/Luigi_inactive.png",
+            name: "Luigi"
+	},
+	'PEACH': {
+	    img: "/static/media/Peach_inactive.png",
+            name: "Peach"
+	},
+	'TOAD': {
+	    img: "/static/media/Toad_inactive.png",
+            name: "Toad"
+	},
+	'YOSHI': {
+	    img: "/static/media/Yoshi_inactive.png",
+            name: "Yoshi"
+	},
+	'DK': {
+	    img: "/static/media/DK_inactive.png",
+            name: "DK"
+	},
+	'WARIO': {
+	    img: "/static/media/Wario_inactive.png",
+            name: "Wario"
+	},
+	'BOWSER': {
+	    img: "/static/media/Bowser_inactive.png",
+            name: "Bowser"
+	}
+    };
+    var nameMap = {};
+    function getCharById(id) {
+	if(nameMap[id]) {
+	    id = nameMap[id];
+	}
+	return CharacterEnum[id];
+    }
+
+    function generatePlayerStr() {
+	var id = numPlayers++;
+	var playerName = "player"+id;
+	nameMap[playerName] = playerName;
+	var pSrc = '/static/media/Person_qmark.png';
+	var itemSrc = '/static/media/Item_empty.png';
+	var playerStatusStr = " \
+	   <div id='player"+id+"'class='playerstatus row-fluid'> \
+	   <img class='char span3' src='"+pSrc+"'> \
+	   <div class='name span3'>"+playerName+"</div> \
+	   <div class='lives span3'>***</div> \
+	   <img class='item span3' src='"+itemSrc+"'>\
+	   </div>";
+	return playerStatusStr;
+    }
+    function insertPlayer() {
+	$('#allplayerstatus').append(generatePlayerStr());
+    }
+    var numEvents = 0;
+    /*
+       action -> [picked up, used, hit, joined, died, won, start]
+       (last is a game state)
+       subject | object -> all players
+       item -> seen in ENUM
+    */
+    function generateEvent(subject, object, item, action) {
+	var id = numEvents ++;
+	var subjObj = getCharById(subject);
+	var objObj = getCharById(object);
+	var itemObj = getItemById(item);
+	var newsBody = '';
+	//Handle display of characters
+	if(action == 'picked up') {
+	    $('#'+subject).find('.item').attr('src',itemObj.img);
+	}
+	else if(action == 'used') {
+	    $('#'+subject).find('.item').attr('src',ItemEnum['EMPTY'].img);
+	}
+	else if(action == 'hit') {
+	    var origStr = $('#'+object).find('.lives').text();
+	    origStr = origStr.substr(0,origStr.length-1);
+	    $('#'+object).find('.lives').text(origStr);
+	}
+	if(action == 'start') {
+	    newsBody = "\
+		<p>The Game has Started</p>\
+		";
+	}
+	else {
+	    if(subjObj) {
+		newsBody = "<img class='subj span2' src='"+subjObj.img+"'>";
+	    }
+	    //Unnamed
+	    else {
+		newsBody = "<div class='subj span2'>"+subject+"</div>";
+	    }
+	    if(itemObj) {
+		newsBody += "<div class='action span3 center'>"+action+"</div>";
+		if(object) {
+		    if(objObj){
+			newsBody += "<img class='obj span2' src='"+objObj.img+"'>";
+		    }
+		    else {
+			newsBody += "<div class='obj span2'>"+object+"</div>";
+		    }
+		    newsBody += "<div class='span1'> with </div>";
+		}
+		newsBody += "<img class='item span2' src='"+itemObj.img+"'>";
+	    }
+	}
+	var newsStr = "\
+	   <div id='news"+id+"' class='newsEvent row-fluid'>\
+	   "+newsBody+"\
+	   </div>";
+	return newsStr;
+    }
+    function insertEvent(subj,obj,item,act) {
+	$('#newsfeed').append(generateEvent(subj,obj,item,act));
+    }
+    insertPlayer();
+    insertPlayer();
+    insertPlayer();
+    insertPlayer();
+    insertEvent("player0", "", "GREEN_SHELL", "picked up");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    insertEvent("MARIO", "LUIGI", "GREEN_SHELL", "hit");
+    $(document).ready(function() {
+	$('.playerstatus').on('click', function(event) {
+	    selectedPlayerId = $(this).attr('id');
+	    $('#myModal').modal();
+	});
+	$('.pselect').on('click', function(event) {
+	    if(selectedPlayerId) {
+		var charId = $(this).attr('id');
+		var charObj = getCharById(charId);
+		nameMap[selectedPlayerId] = charObj.name;
+		var $player = $('#'+selectedPlayerId);
+		$player.find('.char').attr('src',
+		    charObj.img);
+		$player.find('.name').text(charObj.name);
+		selectedPlayerId = null;
+		$('#myModal').modal('toggle');
+	    }
+	});
+    });
+});
+
