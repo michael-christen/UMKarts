20728a9084725337685daba8c80711ffeaf14e9b
Mark Schulte <mjschul@umich.edu>
2013-12-03 14:22:07 -0500

    Adding printing of address to the xbee_printf function to help with the printing

--- listener_server/src/xbee_packet.c
+++ listener_server/src/xbee_packet.c
@@ -3,57 +3,68 @@
 #include "xbee_packet.h"
 #include <stdlib.h>
 
-static int _xbee_printf(const uint8_t * data, uint16_t len) {
-  int err, i;
-  for (i = 0; i < len; i++) {
-    err = printf("%c", data[i]);
-    if (err < 0) {
-      return err;
-    }
-  }
-  err = printf("\n");
-  return err;
+static int _xbee_printf(uint64_t address, const uint8_t * data, uint16_t len) {
+	int err, i;
+	err = printf("0x%" PRIu64 ": ", address);
+	if (err < 0) return err;
+	for (i = 0; i < len; i++) {
+		err = printf("%c", data[i]);
+		if (err < 0) {
+			return err;
+		}
+	}
+	err = printf("\n");
+	return err;
+}
+
+static uint64_t _xbee_tx_packet_get_address(uint8_t * start) {
+	int i;
+	uint64_t ret = 0;
+	for (i = 0; i < sizeof(uint64_t); i++) {
+		ret += (((uint64_t) start[i]) << i*8) & (0xFF << i*8);
+	}
+	return ret;
 }
 
 int xbee_get_next_packet(xbee_serial_t *serial, struct xbee_packet * xp, unsigned char debug) {
-  struct xbee_reader xr;
-  int result;
-  uint8_t ch;
-  XBeeReaderInit(&xr, xp);
-  while (!XBeeReaderDone(&xr)) {
-    result = xbee_ser_getchar(serial);
-    if (result >= 0) {
-      ch = result & 0xFF;
-      if (debug) printf("%c\n", ch);
-      XBeeReaderRead(&xr, &ch, &ch + 1);
-    }
-    else if (result != -ENODATA) {
-      Log(EERROR, "Unable to read packet because %s", strerror(errno));
-    }
-  }
+	struct xbee_reader xr;
+	int result;
+	uint8_t ch;
+	XBeeReaderInit(&xr, xp);
+	while (!XBeeReaderDone(&xr)) {
+		result = xbee_ser_getchar(serial);
+		if (result >= 0) {
+			ch = result & 0xFF;
+			if (debug) printf("%c\n", ch);
+			XBeeReaderRead(&xr, &ch, &ch + 1);
+		}
+		else if (result != -ENODATA) {
+			Log(EERROR, "Unable to read packet because %s", strerror(errno));
+		}
+	}
 
-  if (XBeeReaderGood(&xr)) {
-    return 0;
-  }
-  else {
-    return -1;
-  }
+	if (XBeeReaderGood(&xr)) {
+		return 0;
+	}
+	else {
+		return -1;
+	}
 }
 
 int xbee_received_packet(struct xbee_packet *xp) {
-  uint8_t * data;
-  uint16_t data_len;
-  data_len = xbee_rxpt_get_payload_size(xp);
-  if (data_len > 0) {
-    data = xbee_rxpt_payload_start(xp);
-    switch (*data) {
-      case 0x00:
-        _xbee_printf(data + 1, data_len - 1);
-        break;
-      default:
-        Log(EINFO, "Recevied a tx packet that we have not configured for %x", *data);
-    }
-  }
-  return 0;
+	uint8_t * data;
+	uint16_t data_len;
+	data_len = xbee_rxpt_get_payload_size(xp);
+	if (data_len > 0) {
+		data = xbee_rxpt_payload_start(xp);
+		switch (*data) {
+			case 0x00:
+				_xbee_printf(_xbee_tx_packet_get_address(xp->payload + 1), data + 1, data_len - 1);
+				break;
+			default:
+				Log(EINFO, "Recevied a tx packet that we have not configured for %x", *data);
+		}
+	}
+	return 0;
 }
 
