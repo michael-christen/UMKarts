6957787377a4fbc340663325d225bb9bd476de58
Mark Schulte <schultetwin@gmail.com>
2013-12-04 19:09:12 -0500

    Listener Server: Fixed hex address output

---
+++ listener_server/src/convert.h
@@ -0,0 +1,13 @@
+/*
+ * convert.h
+ */
+
+#include <inttypes.h>
+
+#ifndef CONVERT_H_
+#define CONVERT_H_
+
+uint32_t bytes_to_uint32_t(uint8_t * bytes);
+uint64_t bytes_to_uint64_t(uint8_t * bytes);
+
+#endif /* CONVERT_H_ */
